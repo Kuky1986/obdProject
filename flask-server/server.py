dfc99ea46a134052c30788636109d53a49d22e44
kuks1986 <karlo.kukuruzovi@gmail.com>
2024-03-18 22:10:59 +0000

    display data/paginate Data

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,19 @@
-from flask import Flask
-
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
+from bson.json_util import dumps
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/obd2"
+mongo = PyMongo(app)
 
-@app.route("/members")
-def members():
-    return{"members": ["Member1", "Member2", "Member3"]}
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    page = int(request.args.get('page', 1))  # Get page number from query parameters (default to 1)
+    per_page = int(request.args.get('per_page', 10))  # Get number of items per page (default to 10)
 
-if __name__ == "__main__":
+    cursor = mongo.db.obd_scans.find().skip((page - 1) * per_page).limit(per_page)
+    data = dumps(cursor)  # Convert to JSON using bson.json_util.dumps
+    return data, 200
+
+if __name__ == '__main__':
     app.run(debug=True)
