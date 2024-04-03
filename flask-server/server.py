df9be3be5a88cf4e417fe7b9e36f72c234ef9ad6
kuks1986 <karlo.kukuruzovi@gmail.com>
2024-04-03 12:20:16 +0100

    components/pages and carId paging

--- flask-server/server.py
+++ flask-server/server.py
@@ -8,11 +8,16 @@ mongo = PyMongo(app)
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
-    page = int(request.args.get('page', 1))  # Get page number from query parameters (default to 1)
-    per_page = int(request.args.get('per_page', 10))  # Get number of items per page (default to 10)
+    page = int(request.args.get('page', 1))
+    per_page = int(request.args.get('per_page', 10))
+    car_id = request.args.get('carId')  # Get carId from query parameters
 
-    cursor = mongo.db.obd_scans.find().skip((page - 1) * per_page).limit(per_page)
-    data = dumps(cursor)  # Convert to JSON using bson.json_util.dumps
+    query = {}
+    if car_id:
+        query['VEHICLE_ID'] = car_id
+
+    cursor = mongo.db.obd_scans.find(query).skip((page - 1) * per_page).limit(per_page)
+    data = dumps(cursor)
     return data, 200
 
 if __name__ == '__main__':
